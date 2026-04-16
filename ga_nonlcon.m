ef62c93b95b26c6e124158a6d1e8ec674fabbef9
ljaag <luisjaag@gmail.com>
2026-04-16 10:52:13 +0200

    Delete ga_nonlcon.m

--- ga_nonlcon.m
+++
@@ -1,7 +0,0 @@
-function [c, ceq] = ga_nonlcon(x, params_base, mode)
-%==========================================================================
-% GA_NONLCON
-% Wrapper for GA. Pulls only the constraints 'c' and 'ceq' from the cache.
-%==========================================================================
-    [~, c, ceq] = ga_evaluator_cache(x, params_base, mode);
-end
