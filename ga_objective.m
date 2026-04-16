542fa3db1fc125cb103136bbb760c129bd7bd65c
ljaag <luisjaag@gmail.com>
2026-04-16 10:52:04 +0200

    Delete ga_objective.m

--- ga_objective.m
+++
@@ -1,7 +0,0 @@
-function J = ga_objective(x, params_base, mode)
-%==========================================================================
-% GA_OBJECTIVE
-% Wrapper for GA. Pulls only the objective cost 'J' from the cache.
-%==========================================================================
-    [J, ~, ~] = ga_evaluator_cache(x, params_base, mode);
-end
